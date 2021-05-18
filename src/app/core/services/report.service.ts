d842d045b278078aca276ec2bcc7323fb1529336
Shaeel678 <Shaeel678@gmail.com>
2021-05-18 16:23:13 +0500

    Report Service Created

--- src/app/core/services/report.service.ts
+++ src/app/core/services/report.service.ts
@@ -1,3 +1,4 @@
+import { ApiService } from './api.service';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,27 @@ import { Injectable } from '@angular/core';
 })
 export class ReportService {
 
-  constructor() { }
+  constructor(private _apiService: ApiService) { }
+
+  getReportByClubId(id, offset, limit) {
+    offset = parseInt(offset) < 0 ? 0 : offset;
+    limit = parseInt(limit) < 1 ? 10 : limit;
+    return this._apiService.get(`/report/getByClubID/${id}?offset=${offset}&limit=${limit}`)
+  }
+
+
+  getReportByPostedTo(postedTo, offset, limit) {
+    offset = parseInt(offset) < 0 ? 0 : offset;
+    limit = parseInt(limit) < 1 ? 10 : limit;
+    return this._apiService.get(`/report/getByPostedTo/${postedTo}?offset=${offset}&limit=${limit}`)
+  }
+
+  addReport(report) {
+    return this._apiService.post('/report/addReport', report);
+  }
+
+  updateReport(report){
+    return this._apiService.post('/report/updateReport' , report)
+  }
+
 }
