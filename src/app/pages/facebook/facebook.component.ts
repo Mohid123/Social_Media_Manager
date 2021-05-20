bffa8020b2bf3a4e57d9ab5d402a4d49f7e8eafc
Shaeel678 <Shaeel678@gmail.com>
2021-05-20 16:56:00 +0500

    mat tab switch functionality added

--- src/app/pages/facebook/facebook.component.ts
+++ src/app/pages/facebook/facebook.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
 import { NgxSpinnerService } from "ngx-spinner";
 
 
@@ -13,23 +13,41 @@ export class FacebookComponent implements OnInit {
   constructor(private spinner: NgxSpinnerService) { }
 
  public name = "";
+ url = 'https://getstackposts.com/inc/themes/backend/default/assets/img/avatar.jpg';
+ showDiv = {
+  photo : true,
+  video : false,
+  text : false
+}
  
   ngOnInit() {
-    /** spinner starts on init */
     this.spinner.show();
 
     setTimeout(() => {
-      /** spinner ends after 5 seconds */
       this.spinner.hide();
     }, 1000);
   }
-  url = '';
+  switchTabs(event){
+    if(event.index == 0){
+      this.showDiv.photo = true;
+      this.showDiv.video = false;
+      this.showDiv.text = false;
+    }
+    else if(event.index == 1){
+      this.showDiv.photo = false;
+      this.showDiv.video = true;
+      this.showDiv.text = false;
+    }
+    else {
+      this.showDiv.photo = false;
+      this.showDiv.video = false;
+      this.showDiv.text = true;
+    }
+  }
+
   
-  // showDiv = {
-  //   photo : false,
-  //   video : false,
-  //   text : false
-  // }
+  
+
   onSelectFile(event) {
     if (event.target.files && event.target.files[0]) {
       var reader = new FileReader();
@@ -38,7 +56,7 @@ export class FacebookComponent implements OnInit {
 
       reader.onload = (event) => { // called once readAsDataURL is completed
         this.url = event.target.result as string;
-       
+       console.log(event)
        
       }
     }
