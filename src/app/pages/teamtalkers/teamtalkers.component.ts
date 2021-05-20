bc7551fe1415a50484a8305f7767503c823ec55a
ijunaidkhan <junaidmehmood67@gmail.com>
2021-05-20 18:28:04 +0500

    tabs naviagtion accroding to selected media added to all social media platforms

--- src/app/pages/teamtalkers/teamtalkers.component.ts
+++ src/app/pages/teamtalkers/teamtalkers.component.ts
@@ -9,7 +9,29 @@ import { NgxSpinnerService } from "ngx-spinner";
 export class TeamtalkersComponent implements OnInit {
 
   constructor(private spinner: NgxSpinnerService) { }
-
+  public teamtalkerCaption = "";
+  showDiv = {
+    photo : true,
+    video : false,
+    text : false
+  }
+  switchTabs(event){
+    if(event.index == 0){
+      this.showDiv.photo = true;
+      this.showDiv.video = false;
+      this.showDiv.text = false;
+    }
+    else if(event.index == 1){
+      this.showDiv.photo = false;
+      this.showDiv.video = true;
+      this.showDiv.text = false;
+    }
+    else {
+      this.showDiv.photo = false;
+      this.showDiv.video = false;
+      this.showDiv.text = true;
+    }
+  }
   ngOnInit() {
     /** spinner starts on init */
     this.spinner.show();
