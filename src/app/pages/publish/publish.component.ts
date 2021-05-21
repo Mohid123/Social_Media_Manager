7077eafea259a0b2215f357b1c1067575bd9b2e4
ijunaidkhan <junaidmehmood67@gmail.com>
2021-05-21 14:48:54 +0500

    publish all checklist selection + scroll bar added

--- src/app/pages/publish/publish.component.ts
+++ src/app/pages/publish/publish.component.ts
@@ -7,8 +7,48 @@ import { NgxSpinnerService } from "ngx-spinner";
 })
 export class PublishComponent implements OnInit {
 public textFirst : string
+title = 'Angular 7 CheckBox Select/ Unselect All';
+  masterSelected:boolean;
+  checklist:any;
+  checkedList:any;
   
-constructor(private spinner: NgxSpinnerService , private cf : ChangeDetectorRef) { }
+constructor(private spinner: NgxSpinnerService , private cf : ChangeDetectorRef) { 
+  this.masterSelected = false;
+      this.checklist = [
+        {id:1,value:'Elenor Anderson',isSelected:false},
+        {id:2,value:'Caden Kunze',isSelected:false},
+        {id:3,value:'Ms. Hortense Zulauf',isSelected:false},
+        {id:4,value:'Grady Reichert',isSelected:false},
+        {id:5,value:'Dejon Olson',isSelected:false},
+        {id:6,value:'Jamir Pfannerstill',isSelected:false},
+        {id:7,value:'Aracely Renner DVM',isSelected:false},
+        {id:5,value:'Dejon Olson',isSelected:false},
+        {id:6,value:'Jamir Pfannerstill',isSelected:false},
+        {id:7,value:'Aracely Renner DVM',isSelected:false},
+        {id:8,value:'Genoveva Luettgen',isSelected:false}
+      ];
+      this.getCheckedItemList();
+}
+checkUncheckAll() {
+  for (var i = 0; i < this.checklist.length; i++) {
+    this.checklist[i].isSelected = this.masterSelected;
+  }
+  this.getCheckedItemList();
+}
+isAllSelected() {
+  this.masterSelected = this.checklist.every(function(item:any) {
+      return item.isSelected == true;
+    })
+  this.getCheckedItemList();
+}
+getCheckedItemList(){
+  this.checkedList = [];
+  for (var i = 0; i < this.checklist.length; i++) {
+    if(this.checklist[i].isSelected)
+    this.checkedList.push(this.checklist[i]);
+  }
+  this.checkedList = JSON.stringify(this.checkedList);
+}
 public socialCaption ="";
 public selectedInstagram : boolean = true
 public url : string ='https://getstackposts.com/inc/themes/backend/default/assets/img/avatar.jpg'
