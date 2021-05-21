87e5687e8f205d11398b3e13fba1b898dee69e61
Shaeel678 <Shaeel678@gmail.com>
2021-05-21 10:21:27 +0500

    publish all component file upload function added

--- src/app/pages/publish/publish.component.ts
+++ src/app/pages/publish/publish.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { NgxSpinnerService } from "ngx-spinner";
 @Component({
   selector: 'app-publish',
@@ -8,48 +8,59 @@ import { NgxSpinnerService } from "ngx-spinner";
 export class PublishComponent implements OnInit {
 public textFirst : string
   
-constructor(private spinner: NgxSpinnerService) { }
+constructor(private spinner: NgxSpinnerService , private cf : ChangeDetectorRef) { }
 public socialCaption ="";
-
-url = ""
-textDiv = {
-  text : false
-}
-
-switchTabsText(event){
-
-}
+public selectedInstagram : boolean = true
+public url : string ='https://getstackposts.com/inc/themes/backend/default/assets/img/avatar.jpg'
 showDiv = {
   photo : true,
   video : false,
   text : false
 }
+
+ngOnInit() {
+  this.spinner.show();
+
+  setTimeout(() => {
+    this.spinner.hide();
+  }, 1000);
+}
+
 switchTabs(event){
   if(event.index == 0){
     this.showDiv.photo = true;
     this.showDiv.video = false;
     this.showDiv.text = false;
+    this.selectedInstagram = true;
+
   }
   else if(event.index == 1){
     this.showDiv.photo = false;
     this.showDiv.video = true;
     this.showDiv.text = false;
+    this.selectedInstagram = true;
+
   }
   else {
     this.showDiv.photo = false;
     this.showDiv.video = false;
     this.showDiv.text = true;
+    this.selectedInstagram = false;
   }
 }
-ngOnInit() {
-  /** spinner starts on init */
-  this.spinner.show();
 
-  setTimeout(() => {
-    /** spinner ends after 5 seconds */
-    this.spinner.hide();
-  }, 1000);
+onSelectFile(event) {
+  console.log(event)
+  // console.log(event)
+  if (event.target.files && event.target.files[0]) {
+    var reader = new FileReader();
+    reader.readAsDataURL(event.target.files[0]); // read file as data url
+    reader.onload = (event) => { // called once readAsDataURL is completed
+      this.url = event.target.result as string;
+      this.cf.detectChanges();
+     console.log(event)
+    }
+  }
 }
-
 
 } 
