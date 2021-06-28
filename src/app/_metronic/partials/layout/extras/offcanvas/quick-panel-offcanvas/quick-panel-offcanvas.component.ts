5adc003d3c6f29c182efad3a167ea2ea6f046cab
ijunaidkhan <junaidmehmood67@gmail.com>
2021-06-28 13:27:34 +0500

    notification + live stram toggle added

--- src/app/_metronic/partials/layout/extras/offcanvas/quick-panel-offcanvas/quick-panel-offcanvas.component.ts
+++ src/app/_metronic/partials/layout/extras/offcanvas/quick-panel-offcanvas/quick-panel-offcanvas.component.ts
@@ -1,4 +1,6 @@
+import { ChangeDetectorRef } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { ReportService } from 'src/app/core/services/report.service';
 import { LayoutService } from '../../../../../core';
 
 @Component({
@@ -7,21 +9,38 @@ import { LayoutService } from '../../../../../core';
   styleUrls: ['./quick-panel-offcanvas.component.scss'],
 })
 export class QuickPanelOffcanvasComponent implements OnInit {
+
+  public latestReports: any = []
   extrasQuickPanelOffcanvasDirectionCSSClass = 'offcanvas-right';
   activeTabId:
     | 'kt_quick_panel_logs'
     | 'kt_quick_panel_notifications'
     | 'kt_quick_panel_settings' = 'kt_quick_panel_logs';
 
-  constructor(private layout: LayoutService) {}
+  constructor(private layout: LayoutService, private _reportService: ReportService, private cf: ChangeDetectorRef) {}
 
   ngOnInit(): void {
     this.extrasQuickPanelOffcanvasDirectionCSSClass = `offcanvas-${this.layout.getProp(
       'extras.quickPanel.offcanvas.direction'
     )}`;
+    this.getLatestReports();
+  }
+
+  getLatestReports() {
+    this.setActiveTabId('kt_quick_panel_notifications')
+    let userId = localStorage.getItem('clubUid');
+    this._reportService.getLatestReports(userId).subscribe((reports: any) => {
+      reports.map((singleReport: any) => {
+        let time = new Date(singleReport.postedTime).toLocaleTimeString('fr-FR', { hour: '2-digit', minute: '2-digit' })
+        singleReport.time = time;
+      });
+      this.latestReports = reports;
+      this.cf.detectChanges();
+    })
   }
 
   setActiveTabId(tabId) {
+    debugger;
     this.activeTabId = tabId;
   }
 
