32cbc4da4870ff5a5784f7fed96acb2bf6290afe
Shaeel678 <Shaeel678@gmail.com>
2021-07-16 13:42:13 +0500

    schedule club post complete

--- src/app/app.constatns.ts
+++ src/app/app.constatns.ts
@@ -2,7 +2,7 @@ import { environment } from '../environments/environment';
 
 export const constants = {
     clubApiUrl: localStorage.getItem('selectedClub') ? JSON.parse(localStorage.getItem('selectedClub')).baseURL : "" ,
-    api_url: 'https://socialapi.solissol.com/api/v1/en' ,
+    api_url: 'http://192.168.1.106:3005' ,
     base_url: 'https://graph.facebook.com/',
     app_id: '844857613045687',
     app_secret: '2159f936003b2d66dfa879da95a60da5'
