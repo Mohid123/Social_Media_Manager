fc33c5cf23da15a925e56c9802d7a9af8590c7e7
Shaeel678 <Shaeel678@gmail.com>
2021-08-12 18:13:34 +0500

    picker-model clubs implementation

--- src/app/core/interceptors/http-interceptor.ts
+++ src/app/core/interceptors/http-interceptor.ts
@@ -7,6 +7,7 @@ import { constants } from 'src/app/app.constants';
 export class Interceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let customReq;
+    let club = JSON.parse(localStorage.getItem('selectedClub'));
     let clubBaseUrl =constants.clubApiUrl;
     if(request.url.includes('getAllClubs') || request.url.includes('loginByEmail') ){
       customReq = request.clone()
@@ -14,6 +15,11 @@ export class Interceptor implements HttpInterceptor {
     else if(request.url.includes('socialapi.solissol') || request.url.includes("http://192.168.1.106:3005")   ) {
       customReq = request.clone({
         headers: request.headers.set('Authorization', localStorage.getItem('app-token'))
+      });
+    }
+    else if(request.url.includes(constants.clubApiUrl) && club.isPicker){
+      customReq = request.clone({
+        headers: request.headers.set('Authorization', localStorage.getItem('club-token')).set('clubID' , club.id)
       });
     }
     else if(request.url.includes(constants.clubApiUrl)){
