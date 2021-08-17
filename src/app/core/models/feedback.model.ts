e72d1b6e30cf49946f05318573391260edb61505
ijunaidkhan <junaidmehmood67@gmail.com>
2021-08-17 16:09:27 +0500

    feedback model implemented

---
+++ src/app/core/models/feedback.model.ts
@@ -0,0 +1,10 @@
+export class Feedback{
+    clubId: string;
+    userId: string;
+    type:string
+    deletedCheck: boolean
+    imageUrl: string
+    message: string
+    browser : string
+    email : string
+}
