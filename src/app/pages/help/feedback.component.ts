ab2f6d93a3eb9c4a9f50b7c655fa42078531880f
Shaeel678 <Shaeel678@gmail.com>
2021-09-17 16:43:44 +0500

    email regex added

--- src/app/pages/help/feedback.component.ts
+++ src/app/pages/help/feedback.component.ts
@@ -22,6 +22,7 @@ export class FeedbackComponent implements OnInit {
   userId: string
   club: any
   feedback: Feedback
+  emailRegex : string = '/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;'
   constructor(private spinner: NgxSpinnerService,
     private fb: FormBuilder,
     private cf: ChangeDetectorRef,
@@ -38,12 +39,12 @@ export class FeedbackComponent implements OnInit {
     this.club = JSON.parse(localStorage.getItem('selectedClub'));
     this.userId = localStorage.getItem('userId')
     this.Module.push(this.club.clubName)
+    let emailRegex = new RegExp( '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$');
     this.myForm = this.fb.group({
       name: [localStorage.getItem('userName'), Validators.required],
-      email: ['', [Validators.required, Validators.email]],
+      email: ['', [Validators.required, Validators.email , Validators.pattern(emailRegex)]],
       message: ['', [Validators.required, Validators.minLength(15)]],
       type: ['', [Validators.required]]
-
     });
   }
 
