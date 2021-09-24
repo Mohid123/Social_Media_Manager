83caa76cf8134b9bb172a5dcf804c722edcdcdd6
ijunaidkhan <junaidmehmood67@gmail.com>
2021-09-24 11:19:45 +0500

    load more button added

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -2,7 +2,7 @@ import { environment } from '../environments/environment';
 
 export const constants = {
     clubApiUrl: localStorage.getItem('selectedClub') ? JSON.parse(localStorage.getItem('selectedClub')).baseURL : "" ,
-    api_url: 'http://localhost:3005' ,
+    api_url: 'https://socialapi.solissol.com/api/v1/en' ,
     base_url: 'https://graph.facebook.com/',
     app_id: '132795392262404',
     app_secret: '3485268dc803c40732f47998c447e4b0'
