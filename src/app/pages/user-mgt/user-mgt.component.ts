79decc859651b80a967cf2f89f4763d8d52c114b
ijunaidkhan <junaid.mehmood@solissol.com>
2022-01-19 18:01:34 +0500

    get all users

--- src/app/pages/user-mgt/user-mgt.component.ts
+++ src/app/pages/user-mgt/user-mgt.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { UserManagement } from '@app/core/services/user-management.service';
+import { User } from './../../core/models/user.model';
+import { ApiResponse } from '@app/core/models/response.model';
+import { Subject } from 'rxjs';
+import { distinctUntilChanged, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'user-management',
@@ -6,10 +11,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-mgt.component.scss']
 })
 export class UserMgtComponent implements OnInit {
-
-  constructor() { }
+  destroy$ = new Subject();
+  public users: User;
+  public offset: number = 0;
+  public limit: number = 12;
+  constructor(public userMgt : UserManagement,
+    public cf: ChangeDetectorRef) { }
 
   ngOnInit(): void {
+    this.getUsers(this.offset, this.limit)
+  }
+
+  getUsers(limit, offset){
+    this.userMgt.getAllUsers(limit, offset).pipe(distinctUntilChanged(),takeUntil(this.destroy$)).subscribe((res: ApiResponse<User>)=>{
+      debugger
+     if(!res.hasErrors()){
+       this.users = res.data;
+       this.cf.detectChanges();
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.complete();
+    this.destroy$.unsubscribe();
   }
 
 }
